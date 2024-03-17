520bab35b4b41f9b8bd9295457c75bbc9a3198e1
MAHEDI HASAN <mahedi5096@gmail.com>
2024-03-18 02:38:03 +0600

    add-search-icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,8 +25,11 @@ const Navbar = () => {
                 </div>
 
                 <div className="navbar-end">
-                    <div className="form-control mr-2">
-                        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                    <div className="mr-2">
+                        <label className="input input-bordered flex items-center gap-2">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg> <input type="text" className="grow" placeholder="Search" />
+                        </label>
                     </div>
                     <div className="w-10 ">
                         <img alt="Tailwind CSS Navbar component"
